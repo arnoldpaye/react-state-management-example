5b4f2b83949ac1768c7deac4640be794db822e53
Arnold Paye <arnoldpayet@gmail.com>
2022-01-28 07:23:09 -0400

    install react-mobx-lite

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { CounterButton } from './CounterButton';
+import { Counter } from './Counter';
 import './App.css';
+
+const counter = new Counter();
 
 const App = () => {
   return (
     <>
       <h1>State Management Example</h1>
-      <CounterButton />
+      <CounterButton counter={counter} />
     </>
   );
 };
--- src/CounterButton.js
+++ src/CounterButton.js
@@ -1,17 +1,17 @@
 import { useState } from 'react';
+import { observer } from 'mobx-react-lite';
 
-export const CounterButton = () => {
-  const [numberOfClicks, setNumberOfClick] = useState(0);
+export const CounterButton = observer(({ counter }) => {
   const [incrementBy, setIncrementBy] = useState(1);
 
   return (
     <>
-      <p>You have clicked the button {numberOfClicks} times.</p>
+      <p>You have clicked the button {counter.numberOfClicks} times.</p>
       <label>
         Increment By:
         <input value={incrementBy} onChange={e => setIncrementBy(Number(e.target.value))} type="number" />
       </label>
-      <button onClick={() => setNumberOfClick(numberOfClicks + incrementBy)}>Click</button>
+      <button onClick={() => counter.increment(incrementBy)}>Click</button>
     </>
   )
-};
+});
